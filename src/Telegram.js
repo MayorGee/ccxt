af01733fb665e7c3965704d2105142c43f64679a
mayorCoder <adebayomayowa78@gmail.com>
2023-01-18 01:43:17 +0300

    Upgraded telegram chat functionality

--- src/Telegram.js
+++ src/Telegram.js
@@ -1,5 +1,7 @@
-import TelegramBot from "node-telegram-bot-api";
-import Environment from "./Environment.js"; 
+import axios from 'axios';
+
+import TelegramBot from 'node-telegram-bot-api';
+import Environment from './Environment.js'; 
 
 export default class Telegram {
     bot;
@@ -14,9 +16,37 @@ export default class Telegram {
 
     initBot() {
         this.bot.on('message', (message) => {
-            console.log(message.text);
+            switch (message.text) {
+                case '/start':
+                    this.bot.sendMessage(this.chatId, 'Hello! I am AgbaCryptoBot, what can I do for you today?');
+                    break;
 
-            this.bot.sendMessage(this.chatId, 'Message succesfully sent to console')
+                case '/stop':
+                    this.bot.sendMessage(this.chatId, 'GoodBye! Hope to see you soon');
+                    break;
+
+                default:
+                    // this.bot.sendMessage(this.chatId, `I don't understand this, check for any typo and try again please`);
+                    break;
+            }
         });
+
+        this.bot.onText(/\/price (.+)/, async (message, data) => {
+            try {
+                let ticker = data[1];
+            
+                const price = await this.getCurrencyPrice(ticker);
+            
+                this.bot.sendMessage(this.chatId, price);
+            } catch (error) {
+                this.bot.sendMessage(this.chatId, 'Error getting crypto price!, check for typos and try again please');
+            }
+        });
+    }
+
+    async getCurrencyPrice(ticker) {
+        const price = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${ticker}&vs_currencies=usd`);
+
+        return price.data[ticker].usd;
     }
 }
