45630d3d39f5a600a2fb9d72052a4bf55ed3bd7b
mayorCoder <adebayomayowa78@gmail.com>
2023-01-23 22:20:00 +0300

    Updated Start Command

--- src/Telegram.js
+++ src/Telegram.js
@@ -18,23 +18,44 @@ export default class Telegram {
         this.bot.on('message', (message) => {
             switch (message.text) {
                 case '/start':
-                    this.bot.sendMessage(this.chatId, 'Hello! I am AgbaCryptoBot, what can I do for you today?');
+                    this.bot.sendMessage(
+                        this.chatId, 
+                        'Hello! I am AgbaCryptoBot, what can I do for you today?',
+                        {
+                            'reply_markup': {
+                                'keyboard': [
+                                    [
+                                        '/price', 
+                                        '/trade'
+                                    ],   
+                                    ['/performance'],
+                                    [
+                                        '/table',
+                                        '/balance',
+                                        '/blyat'
+                                    ]
+                                ]
+                            }
+                        });
+
                     break;
 
                 case '/stop':
                     this.bot.sendMessage(this.chatId, 'GoodBye! Hope to see you soon');
+                    
                     break;
 
                 default:
-                    // this.bot.sendMessage(this.chatId, `I don't understand this, check for any typo and try again please`);
+                    this.bot.sendMessage(this.chatId, `I don't understand this, check for any typo and try again please`);
+                    
                     break;
             }
         });
 
+        // Matches '/price [symbol]'
         this.bot.onText(/\/price (.+)/, async (message, data) => {
             try {
                 let ticker = data[1];
-            
                 const price = await this.getCurrencyPrice(ticker);
             
                 this.bot.sendMessage(this.chatId, price);
